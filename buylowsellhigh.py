dc9c270d59642f9504a8d38bf13cecc0d19791d1
root <DennisWanjeri>
2021-06-02 08:58:15 +0000

    matplotlib

--- buylowsellhigh.py
+++ buylowsellhigh.py
@@ -2,6 +2,10 @@
 """buy low sell high"""
 from pandas_datareader import data
 import pandas as pd
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 
 start_date = '2014-01-01'
@@ -11,4 +15,16 @@ goog_data = data.DataReader('GOOG', 'yahoo', start_date, end_date)
 goog_data_signal = pd.DataFrame(index=goog_data.index)
 goog_data_signal['price'] = goog_data['Adj Close']
 goog_data_signal['daily_difference'] = goog_data_signal['price'].diff()
-print(goog_data_signal)
+goog_data_signal['signal'] = 0.0
+goog_data_signal['signal'] = np.where(goog_data_signal['daily_difference'] > 0, 1.0, 0.0)
+goog_data_signal['positions'] = goog_data_signal['signal'].diff()
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111, ylabel='Google price in $')
+goog_data_signal['price'].plot(ax=ax1, color='r', lw=2.)
+ax1.plot(goog_data_signal.loc[goog_data_signal.positions == 1.0].index,
+         goog_data_signal.price[goog_data_signal.positions == 1.0], '^', markersize=5, color='m')
+ax1.plot(goog_data_signal.loc[goog_data_signal.positions == -1.0].index,
+         goog_data_signal.price[goog_data_signal.positions == -1.0],
+         'v', markersize=5, color='k')
+plt.savefig('image.png')
