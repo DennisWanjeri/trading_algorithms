852c4d28c6bf04bd9bdfe4d8a495bc7cdecdb799
root <DennisWanjeri>
2021-06-02 11:11:20 +0000

    analysis

--- buylowsellhigh.py
+++ buylowsellhigh.py
@@ -8,6 +8,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
+"""preparing the data signal"""
 start_date = '2014-01-01'
 end_date = '2018-01-01'
 goog_data = data.DataReader('GOOG', 'yahoo', start_date, end_date)
@@ -19,8 +20,12 @@ goog_data_signal['signal'] = 0.0
 goog_data_signal['signal'] = np.where(goog_data_signal['daily_difference'] > 0, 1.0, 0.0)
 goog_data_signal['positions'] = goog_data_signal['signal'].diff()
 
+
+"""visualizing the data signal using matplotlib plot"""
 fig = plt.figure()
 ax1 = fig.add_subplot(111, ylabel='Google price in $')
+
+
 goog_data_signal['price'].plot(ax=ax1, color='r', lw=2.)
 ax1.plot(goog_data_signal.loc[goog_data_signal.positions == 1.0].index,
          goog_data_signal.price[goog_data_signal.positions == 1.0], '^', markersize=5, color='m')
@@ -28,3 +33,18 @@ ax1.plot(goog_data_signal.loc[goog_data_signal.positions == -1.0].index,
          goog_data_signal.price[goog_data_signal.positions == -1.0],
          'v', markersize=5, color='k')
 plt.savefig('image.png')
+
+"""Backtesting:the past predicts the future, testing our strategy with an initial capital over a period of time"""
+initial_capital = float(1000.0)
+
+#dataframe for positions and portfolio
+positions = pd.DataFrame(index=goog_data_signal.index).fillna(0.0)
+portfolio = pd.DataFrame(index=goog_data_signal.index).fillna(0.0)
+
+positions['GOOG'] = goog_data_signal['signal']
+
+#amount of the GOOG positions for the portfolio
+portfolio['positions'] = (positions.multiply(goog_data_signal['price'], axis=0))
+#non invested money
+portfolio['cash'] = initial_capital - (positions.diff().multiply(goog_data_signal['price'], axis=0)).cumsum()
+portfolio['total'] = portfolio['positions'] + portfolio['cash']
